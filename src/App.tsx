4cd6f7c462de96b3e43c0252f3daa5ff1f814802
Brixal-B <121498923+Brixal-B@users.noreply.github.com>
2025-08-11 20:02:52 -0400

    Scaffold standard React + TypeScript starter

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,12 @@
-// Content of App.tsx
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to MTG Investment</h1>
+      <p>This is a React + TypeScript starter.</p>
+    </div>
+  );
+}
+
+export default App;
